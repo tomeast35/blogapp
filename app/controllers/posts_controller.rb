c5a1075bcab8241d22f4eef69bdd851fca1689c4
Tooya Higashi <tomoyahigashi.sf@gmail.com>
2015-08-16 15:55:13 +0900

    7.Post新規作成画面を作成する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,22 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+
+	def new
+		@post = Post.new
+
+	end 
+
+	def create
+		@post = Post.new(post_params)
+		@post.save
+		redirect_to posts_path
+	end
+
+	private
+	def post_params
+		params[:post].permit(:title)
+	end
+
+
 end
