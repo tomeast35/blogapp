392d30675582a0454e8681a64fa8ae9d1e1b7920
Tooya Higashi <tomoyahigashi.sf@gmail.com>
2015-08-16 17:05:51 +0900

    12. Postsコントローラー内のProject.find(params[:id])の重複をなくす

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+
+	 before_action :set_post, only: [:show, :edit, :update, :destroy]
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -23,11 +24,9 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
 		if @post.update(post_params)
 		redirect_to posts_path
 		else
@@ -36,7 +35,6 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to posts_path
 	end
@@ -45,6 +43,10 @@ class PostsController < ApplicationController
 	private
 	def post_params
 		params[:post].permit(:title, :description)
+	end
+
+	def set_post
+		@post = Post.find(params[:id])
 	end
 
 end
