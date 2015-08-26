19ed193aab76260977230f4ae11dda481554543e
Tooya Higashi <tomoyahigashi.sf@gmail.com>
2015-08-26 14:29:18 +0900

    Createアクションの修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
 	belongs_to :post
 	validates :body, 
 	presence: { message: "入力して下さい"}
+
+	
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,5 @@
 class Post < ActiveRecord::Base
 	has_many :comments
-	validates :body, 
-	presence: { message: "入力して下さい"}
-
 
 	validates :title, 
 	presence: { message: "入力して下さい"}
